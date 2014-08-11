982a5f8f22f1426e06f5a211194a914648b11e33
Salah N <salah@3cedz.com>
2014-08-11 17:42:16 +0100

    ajouts dans db.php

--- config/db.php
+++ config/db.php
@@ -1,35 +1,45 @@
 <?php 
 
-//  hostname de la base de données, par defaut localhsot
-$mysql_hostname = "localhost"; 
+	require_once('config.php');
+	
+	## DATABASE
 
+	if (($_SERVER['SERVER_ADDR'] == "127.0.0.1") OR ($_SERVER['SERVER_ADDR'] == "::1"))
+	{
+		// home / development environment
+		$host = 'localhost';
+		$db = 'uabt'; 
+		$baseURL = 'http://localhost/';
+		//$user = 'root';
+		//$password = '123';
+		$user = 'Vbeta1';
+		$password = 'hhhhh';
+	}
+	else 
+	{
+		// server / web host
+		$host = 'localhost';
+		$db = 'uabt';
+		$baseURL = $siteURL.'/'; //creer la variable siteURL dans le fichier config.php
+		$user = 'user';
+		$password = 'pass';
+	}
 
+	// se connecter a la BDD
+	if (!isset($conn )) $conn = mysql_connect($host, $user, $password) OR die("<h1>Erreure de connection a la BDD. ".mysql_error()."</h1>");
 
-//  utilisateur de la base de données
-$mysql_utilisateur = "Vbeta1";
+	// configuration des caracteres
+	//mysql_set_charset('utf8',$conn); // activer pour les anciennes versions de PHP/MySQL
+	mysql_query("
+		SET NAMES 'utf8',
+		SET character_set_results = 'utf8', 
+		character_set_client = 'utf8', 
+		character_set_connection = 'utf8', 
+		character_set_database = 'utf8', 
+		character_set_server = 'utf8'", 
+		$conn);
+	
+	// selectionner la BDD
+	mysql_select_db($db, $conn) or die("<h2>Impossible de selectionner la BDD ".$db."</h2><p>".mysql_error()."</p>");
 
-
-
-//  mot de passe d'utilisateur de la abase de donnée
-$mysql_pass = "hhhhh";  
-
-
-
-// nom de la base de données
-$mysql_database = "uabt";  
-
-
-
-//  etablir la connexion a la base de donnée
-$bd = mysql_connect($mysql_hostname, $mysql_utilisateur, $mysql_pass) or die("Erreur de connexion"); 
-
-
-
-//  selectionner la base de données
-mysql_select_db($mysql_database, $bd) or die("Erreur de connexion");
-
-
-
-//  encoder les caractéres envoyées a la base de données
-mysql_query("SET NAMES 'utf8'");  
- ?>
+?>
